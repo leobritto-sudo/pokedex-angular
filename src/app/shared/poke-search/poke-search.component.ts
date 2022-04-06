01a36e3fb7245d1319214bc40eb4f4590919ee0a
Leonardo Alves <littocraft@gmail.com>
2022-04-05 22:56:34 -0300

    Pikachu I choose you

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss']
+})
+export class PokeSearchComponent implements OnInit {
+
+  @Output() public emitSearch: EventEmitter<string> = new EventEmitter
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public search(value: string) {
+    this.emitSearch.emit(value)
+  }
+
+}
